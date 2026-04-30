2926fa414db44982c64faa3d77d2ea6c7372e75e
alphieke11y <alphonsus4lifensoul@gmail.com>
2026-04-30 09:48:38 +0100

    add

--- frontend/src/lib/api/apiClient.ts
+++ frontend/src/lib/api/apiClient.ts
@@ -56,8 +56,18 @@ export async function apiClient<TResponse, TBody = unknown>(
     let message = 'API Request failed';
 
     if (typeof parsed === 'object' && parsed !== null) {
-      const p = parsed as Record<string, unknown>; //const p = parsed as { message?: string; error?: string };
-      message = p.message || p.error?.message || JSON.stringify(p) || message; //message = p.message || p.error || message;
+      if (typeof parsed === 'object' && parsed !== null) {
+		  const p = parsed as {
+			message?: string;
+			error?: { message?: string } | string;
+		  };
+
+		  message =
+			p.message ||
+			(typeof p.error === 'object' ? p.error?.message : p.error) ||
+			JSON.stringify(p) ||
+			message;
+		}
     } else if (typeof parsed === 'string') {
       message = parsed;
     }
